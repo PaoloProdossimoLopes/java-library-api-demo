7f0f2c416646fd65972114cd8ba775bc6e2a822d
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-03-16 19:36:29 -0300

    chore: setup mavem project with spring boot

---
+++ library-api-demo/src/main/java/com/paoloprodossimolopes/libraryapidemo/LibraryApiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.paoloprodossimolopes.libraryapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApiDemoApplication.class, args);
+	}
+}
