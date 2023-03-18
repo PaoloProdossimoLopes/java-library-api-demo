79ba383c36b230745393790bd7cbb2aa62e130f1
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-03-18 09:54:17 -0300

    feat: create a singleton ModelMapper injected dependency with annotation Bean

--- library-api-demo/src/main/java/com/paoloprodossimolopes/libraryapidemo/LibraryApiDemoApplication.java
+++ library-api-demo/src/main/java/com/paoloprodossimolopes/libraryapidemo/LibraryApiDemoApplication.java
@@ -1,10 +1,22 @@
 package com.paoloprodossimolopes.libraryapidemo;
 
+import com.paoloprodossimolopes.libraryapidemo.api.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryApiDemoApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+//	@Bean
+//	public BookService bookService() {}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiDemoApplication.class, args);
